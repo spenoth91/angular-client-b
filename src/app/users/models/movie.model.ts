e5fbb8e343a0f9441acda0ea147d1ebd401911c6
Drágos Norbert <79311672+dragosnorbert439@users.noreply.github.com>
2022-05-30 12:11:48 +0300

    Chore: Added Movie Model (basic)
    + will have to change it later

---
+++ src/app/users/models/movie.model.ts
@@ -0,0 +1,10 @@
+export class Movie {
+  id: string;
+  l: string; // title
+  //id: number;
+  //title: string;
+  //duration: number;
+  //year: number;
+  //director: string;
+  //category: string;
+}
