6ee54a0f0efd0bc3171d21278facd82afda065d3
Drágos Norbert <79311672+dragosnorbert439@users.noreply.github.com>
2022-05-31 15:24:54 +0300

    Chore: Added register to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import {UserListComponent} from './users/components/user-list/user-list.component';
 import {AuthGuard} from "./account/components/services/auth.guard";
 import {LoginComponent} from "./account/components/login/login.component";
+import {RegisterComponent} from "./account/components/register/register.component";
 
 
 const routes: Routes = [
   { path: 'users', component: UserListComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
