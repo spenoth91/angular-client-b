85b873d4e07ce2d0c62198a698cc0b86d1545d69
Drágos Norbert <79311672+dragosnorbert439@users.noreply.github.com>
2022-05-31 14:59:48 +0300

    Chore: Changed Login component class
    + changed to the one given from the template

--- src/app/account/components/login/login.component.ts
+++ src/app/account/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {ActivatedRoute, Router} from "@angular/router";
 import {AccountService} from "../services/account.service";
 
@@ -19,6 +19,22 @@ export class LoginComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
+  get f() { return this.form.controls; }
+
+  onSubmit() {
+    if (this.form.invalid) return;
+
+    this.accountService.login(this.f.email.value, this.f.password.value)
+      .subscribe((result) =>{
+        if (result) {
+          this.router.navigate(['/']);
+        }
+      })
+  }
 }
