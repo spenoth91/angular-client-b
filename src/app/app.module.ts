0fc0382ac86ff54b31bfeb592648e80724728bb2
Drágos Norbert <79311672+dragosnorbert439@users.noreply.github.com>
2022-05-31 14:13:06 +0300

    Chore: Added LoginComponent import to App.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { UserListComponent } from './users/components/user-list/user-list.component';
 import {TableModule} from 'primeng/table';
+import { LoginComponent } from './account/components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent
+    UserListComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
